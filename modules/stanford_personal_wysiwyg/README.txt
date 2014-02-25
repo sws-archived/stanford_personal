731348180abfb8a889a49e2d4a8199dded6c7a07
John Bickar <jbickar@stanford.edu>
2014-02-25 12:48:35 -0800

    First commit of port of stanford_jumpstart modules. Probably not working 100%

---
+++ modules/stanford_personal_wysiwyg/README.txt
@@ -0,0 +1,6 @@
+Stanford Jumpstart WYSIWYG
+==========================
+
+Out-of-the-box base WYSIWYG configuration for Stanford Sites Jumpstart
+
+
---
+++ templates/launchchecklist.tpl.php
@@ -0,0 +1,14 @@
+<h2>Launch Checklist</h2>
+<ul>
+	<li dir="ltr"><strong>Content:</strong> Have you renamed any starter pages that aren&rsquo;t relevant to your site? Has all placeholder text in starter pages been replaced?</li>
+	<li dir="ltr"><strong>Blocks:</strong> Have you hidden any blocks that aren&rsquo;t relevant to your site? Do your homepage and footer blocks have the correct content in them?</li>
+	<li dir="ltr"><strong>Images:</strong> Are all placeholder images relevant to your site or have they been replaced? Do all images have short descriptive alternate text?</li>
+	<li dir="ltr"><strong>Links:</strong> Are all &ldquo;Read More&rdquo; links and links within content correct? Are all of your menu links correct?</li>
+	<li dir="ltr"><strong>URL Redirects:</strong> Inventory the URLs from your old site to build redirects, so that those old links will still work after launch. Here is an <a target="_blank" href="https://stanford.box.com/s/h85hy9rgqhfaks2no07m">Example Spreadsheet</a>.</li>
+	<li dir="ltr"><strong>Vanity URL:</strong> Do you already have, or have you requested, a <a target="_blank" href="https://itservices.stanford.edu/service/vhost">Vanity URL</a>?</li>
+	<li dir="ltr"><strong>Approval: </strong>Does your site need to be approved by anyone other than you?</li>
+</ul>
+<h2>Are you ready to launch your site?</h2>
+<p>Once you have gone through the launch checklist above, you can request to launch your site. Launching your site means that it will be assigned a Vanity URL (such as http://mysite.stanford.edu) and it will be made available to search engines.</p>
+<p><a href="https://stanforduniversity.qualtrics.com/SE/?SID=SV_01I4MJkFACIhhIN" target="_blank" class="btn btn-request">Launch Your Site</a></p>
+
