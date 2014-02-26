438605d534a8dde6df5cbba73e46a105de1d99f2
John Bickar <jbickar@stanford.edu>
2014-02-26 12:23:07 -0800

    First pass at Get Help text

--- templates/gethelp.tpl.php
+++ templates/gethelp.tpl.php
@@ -3,5 +3,46 @@
  * @file
  * Template file for "Get Help" page
  */
-
-
+?>
+<h1>Get Help</h1>
+<div class="block-extrainfo block-need-assistance">
+<h2>Stanford Sites Personal User Guide</h2>
+<p>Do you need help with Stanford Sites Jumpstart? Look no further! We have lots of helpful tips in our user guide.</p>
+<p><a href="https://sites.stanford.edu/personal-userguide/user-guide" class="btn btn-request" target="blank">View  Guide</a></p>
+</div>
+<h2>Help by category:</h2>
+<ul>
+  <li><a href="https://sites.stanford.edu/personal-userguide/user-guide/your-new-site" target="_blank">Your new site</a>
+    <br>
+  First steps for getting up and running</li>
+  <li>
+    <a href="https://sites.stanford.edu/personal-userguide/user-guide/pages" target="_blank">Pages</a>
+    <br>
+    Editing your pages and main menu
+  </li>
+  <li>
+    <a href="https://sites.stanford.edu/personal-userguide/user-guide/blocks" target="_blank">Blocks</a>
+    <br>
+    Editing your profile information, social media, sidebar, and footer blocks</li>
+  <li>
+    <a href="https://sites.stanford.edu/personal-userguide/user-guide/content-editor" target="_blank">Content editor</a>
+    <br>
+    Formatting your page content
+  </li>
+  <li><a href="https://sites.stanford.edu/personal-userguide/user-guide/multimedia" target="_blank">Multimedia</a>
+    <br>
+    Adding imagery, videos, and audio</li>
+  <li>
+    <a href="https://sites.stanford.edu/personal-userguide/user-guide/design" target="_blank">Design</a>
+    <br>
+    Changing the layout, color and typrography
+  </li>
+  <li>
+    <a href="https://sites.stanford.edu/personal-userguide/user-guide/users" target="_blank">Users</a>
+    <br>
+    Adding additional editors
+  </li>
+</ul>
+<h2>Something really broken?</h2>
+<p>Help us improve our service. Is there something not working properly or did you accidentally explode your site?</p>
+<a href="http://helpsu.stanford.edu/helpsu.cgi?pcat=webdesign" class="btn btn-request" target="blank">Submit a HelpSU ticket</a></strong>
